7242c53351ee264161704286ca63570e77f8ac9c
Brian Delgado <braiandelgado4255@hotmail.com>
2022-02-19 13:22:59 -0300

    First commit

---
+++ src/App/index.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../Components/Navbar";
+import HeroSection from "../Components/HeroSection";
+import Footer from "../Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
